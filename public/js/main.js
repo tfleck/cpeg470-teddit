8ee00441e032348c1afc4e158d99d74a9310d86f
T Fleck <emailtfleck@gmail.com>
2019-10-24 22:46:15 -0400

    fix signin, add mvp layout

--- public/js/main.js
+++ public/js/main.js
@@ -9,76 +9,57 @@ var firebaseConfig = {
   appId: "1:349139338610:web:38d40e0cf839a77b77c9eb",
   measurementId: "G-HRXWVK410N"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
-var db = firebase.firestore();
-//var google_provider = new firebase.auth.GoogleAuthProvider();
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
+$(document).ready(function(){
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var db = firebase.firestore();
+  var google_provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    window.user = user;
+    if(window.user === null){
+      $('#loginBtn').html("Log In");
+      $('#loginBtn').removeClass('btn-outline-success');
+      $('#loginBtn').addClass('btn-success');
     }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'redirect',
-  //signInSuccessUrl: '<url-to-redirect-to-on-success>',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
-};
-
-ui.start('#firebaseui-auth-container', uiConfig);
-
-/*
-function signIn(){
-  firebase.auth().signInWithRedirect(google_provider);
-}
-
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-  console.log(user);
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-*/
-
-
-db.collection("threads").doc("thread1").collection("posts").get().then((querySnapshot) => {
-  querySnapshot.forEach(function(doc) {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
+    else{
+      $('#loginBtn').html(window.user.displayName);
+      $('#loginBtn').removeClass('btn-success');
+      $('#loginBtn').addClass('btn-outline-success');
+    }
+    console.log(user);
   });
 
-  //console.log(doc);
-  //console.log(doc.data());
+  db.collection("threads").doc("thread1").collection("posts").get().then((querySnapshot) => {
+    querySnapshot.forEach(function(doc) {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  });
+  
+  //disable browser defaults for enter key to not screw with search box submit
+  $(document).on("keydown", ":input", function(event) {
+    if (event.key == "Enter") {
+      event.preventDefault();
+    }
+  });
+
+  $('#loginBtn').click(function(){
+    if($('#loginBtn').html() === "Log In"){
+      firebase.auth().signInWithRedirect(google_provider);
+    }
+    else{
+      firebase.auth().signOut();
+    }
+  });
+
+  $('#searchBox').on('keyup', function(e){
+    if(e.key === "Enter"){
+      console.log("enter");
+    }
+  })
 });
