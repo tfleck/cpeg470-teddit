a9b2ee0727ca33b847b494a4ec751e2bba8bbc57
Theo Fleck <emailtfleck@gmail.com>
2019-10-25 19:30:59 -0400

    dynamically fill content from database based on url

--- public/js/main.js
+++ public/js/main.js
@@ -10,8 +10,17 @@ var firebaseConfig = {
   measurementId: "G-HRXWVK410N"
 };
 
+//get URL parameters
+const pathnameSplit = window.location.pathname.split('/');
+const pathSegments = pathnameSplit.filter(function(value, index, arr){
+  return value !== '';
+});
 
 $(document).ready(function(){
+  
+  console.log(pathnameSplit);
+  console.log(pathSegments);
+  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
@@ -33,13 +42,18 @@ $(document).ready(function(){
     }
     console.log(user);
   });
-
-  db.collection("threads").doc("thread1").collection("posts").get().then((querySnapshot) => {
+  
+  if(pathSegments[0] === 't'){
+    db.collection("threads").doc(pathSegments[1]).collection("posts").get().then((querySnapshot) => {
+    $('#mainContainer').html('');
     querySnapshot.forEach(function(doc) {
       // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
+      //console.log(doc.id, " => ", doc.data());
+      addPost(doc.data());
     });
   });
+  }
+  
   
   //disable browser defaults for enter key to not screw with search box submit
   $(document).on("keydown", ":input", function(event) {
@@ -63,3 +77,42 @@ $(document).ready(function(){
     }
   })
 });
+
+function addPost(data){
+  //turn elapsed seconds into whole number of appropriate unit
+  let elapsedTime = (Date.now()/1000-data.time.seconds)/60;
+  let elapsedString = "minutes";
+  if(elapsedTime > 60){
+    elapsedTime = elapsedTime/60;
+    elapsedString = "hours";
+    if(elapsedTime > 24){
+      elapsedTime = elapsedTime/24;
+      elapsedString = "days";
+    }
+  }
+  elapsedTime = Math.round(elapsedTime);
+  if(elapsedTime == 1){
+    //remove trailing s if not needed
+    elapsedString = elapsedString.substr(0,elapsedString.length-1);
+  }
+  $('#mainContainer').append(`
+    <br>
+    <div class="container-fluid">
+    <div class="row justify-content-center">
+          <div class="col-10 col-md-8">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="card-title">${data.title}</h5>
+              </div>
+              <div class="card-body">
+                <p class="card-text">${data.body}</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">${data.authorDisplay} last updated ${elapsedTime} ${elapsedString} ago</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  `)
+}
